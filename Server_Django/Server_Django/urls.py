715ec67521299576c74e050f086f2f97232d84d0
Wolfgang Vinícius Manzato <108307517+WolfgangVM@users.noreply.github.com>
2024-09-29 14:40:35 -0300

    editando
    começando a configurar o django

--- Server_Django/Server_Django/urls.py
+++ Server_Django/Server_Django/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Projet project.
+URL configuration for Server_Django project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home),
 ]
