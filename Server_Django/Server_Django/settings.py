5a73da9da3ee55d59b3be05b6ab585ad357c8abb
Wolfgang Vinícius Manzato <108307517+WolfgangVM@users.noreply.github.com>
2024-10-10 22:29:58 -0300

    adicionado o banco de dados no django

--- Server_Django/Server_Django/settings.py
+++ Server_Django/Server_Django/settings.py
@@ -75,13 +75,26 @@ WSGI_APPLICATION = 'Server_Django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "bancodadospi",
+        "USER": "wolfgang",
+        "PASSWORD": "flow7419663",
+        "HOST": "localhost",
+        "PORT": "3306",
+        'OPITIONS':{
+        'charset': 'utf8mb4',
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
