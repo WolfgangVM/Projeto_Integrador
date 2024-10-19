96c215b3e784f6d29f7322eda1282eee6be86fed
Wolfgang Vinícius Manzato <108307517+WolfgangVM@users.noreply.github.com>
2024-10-19 17:14:53 -0300

    adicionando mapa INCOMPLETO

--- Server_Django/home/views.py
+++ Server_Django/home/views.py
@@ -6,6 +6,6 @@ import json
 def home(request):
     return render(request, 'index.html')
 
-def mapa_view(request):
-    locations = list(Location.objects.values('nome', 'latitude', 'longitude'))
-    return render(request, 'index.html', {'locations': json.dumps(locations)})
+#def home_view(request):
+#    locations = list(Location.objects.values('nome', 'latitude', 'longitude'))
+#S    return render(request, 'index.html', {'locations': json.dumps(locations)})
