3a68eb0b0654d1e4c47f30105d0a05c2d2cd8228
Wolfgang Vinícius Manzato <108307517+WolfgangVM@users.noreply.github.com>
2024-10-19 16:50:30 -0300

    iniciando a adição do mapa

--- Server_Django/home/models.py
+++ Server_Django/home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class locations(models.Model):
+    nome = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longetude = models.FloatField()
+
+def __str__(self):
+    return self.nome
--- Server_Django/home/views.py
+++ Server_Django/home/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from .models import locations
+import json
 
 def home(request):
     return render(request, 'index.html')
+
+def mapa_view(request):
+    locations = list(Location.objects.values('nome', 'latitude', 'longitude'))
+    return render(request, 'index.html', {'locations': json.dumps(locations)})
