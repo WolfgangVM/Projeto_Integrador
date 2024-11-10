8c31793f85b66b5a23cf2b9de5849e1fe9d06a8f
Wolfgang Vinícius Manzato <108307517+WolfgangVM@users.noreply.github.com>
2024-11-10 18:26:42 -0300

    em comenetarios tem codigos que não consegui rodar da forma esperada, caso queira dar uma olhada

--- Server_Django/home/static/js/mapa.js
+++ Server_Django/home/static/js/mapa.js
@@ -30,12 +30,14 @@ map.on('click', onMapClick);
 
 
 
-
-
             //pontos aadicionados manualmente
 var marker = L.marker([-22.348923636964468, -48.77864794069507],
     {alt: 'Pederneiras'}).addTo(map)
     .bindPopup('Cidade de Pederneiras');
+
+var marker = L.marker([-22.3505, -48.780789],
+    {alt: 'Pederneiras'}).addTo(map)
+    .bindPopup('Local para descarte de Metal,Vidro, Plastico, Papelão e Papel');
 
 var marker = L.marker([-22.320307, -49.070382],
     {alt: 'Bauru'}).addTo(map)
@@ -43,11 +45,13 @@ var marker = L.marker([-22.320307, -49.070382],
 
 
 
-    // var littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.'),
-    // denver    = L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.'),
-    // aurora    = L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.'),
-    // golden    = L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.');
-//         // Mobile 
+ var littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.'),
+    denver    = L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.'),
+    aurora    = L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.'),
+    golden    = L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.');
+
+
+        // Mobile 
 
 // var popup = L.popup();
 
@@ -86,3 +90,44 @@ var marker = L.marker([-22.320307, -49.070382],
 // }
 
 // map.on('locationerror', onLocationError);
+
+
+         //layers control
+// var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '© OpenStreetMap'
+// });
+
+// var osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'});
+
+// var map = L.map('map', {
+//     center: [39.73, -104.99],
+//     zoom: 10,
+//     layers: [osm, cities]
+// });
+// var baseMaps = {
+//     "OpenStreetMap": osm,
+//     "OpenStreetMap.HOT": osmHOT
+// };
+
+// var overlayMaps = {
+//     "Cities": cities
+// };
+// var baseMaps = {
+//     "OpenStreetMap": osm,
+//     "<span style='color: red'>OpenStreetMap.HOT</span>": osmHOT
+// };
+// var crownHill = L.marker([39.75, -105.09]).bindPopup('This is Crown Hill Park.'),
+//     rubyHill = L.marker([39.68, -105.00]).bindPopup('This is Ruby Hill Park.');
+    
+// var parks = L.layerGroup([crownHill, rubyHill]);
+// var openTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: 'Map data: © OpenStreetMap contributors, SRTM | Map style: © OpenTopoMap (CC-BY-SA)'
+// });
+
+// layerControl.addBaseLayer(openTopoMap, "OpenTopoMap");
+// layerControl.addOverlay(parks, "Parks");
+            
